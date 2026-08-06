8fa4aa624e93915a6c8d2f0f91cf3c165524fe99
Bill Davidowksy <0_moose_0@proton.me>
2026-08-06 18:37:56 -0400

    Add calculator unit tests to GitHub Actions

---
+++ week10/calculator/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -0,0 +1,23 @@
+package com.leszko.calculator;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void sumsPositiveNumbers() {
+        assertEquals(5, calculator.sum(2, 3));
+    }
+
+    @Test
+    public void sumsWithZero() {
+        assertEquals(7, calculator.sum(7, 0));
+    }
+
+    @Test
+    public void sumsNegativeNumbers() {
+        assertEquals(-5, calculator.sum(-2, -3));
+    }
+}
